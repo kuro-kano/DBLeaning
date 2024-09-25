082f2ad300f58a0f26575890991c1f1036fa492f
Ka-no <138846884+kuro-kano@users.noreply.github.com>
2024-09-25 10:38:38 +0700

    fixed: Lab02-DDL-Including constraints
    wrong file LMAO

--- Assignment/Lab02-DDL-Including constraints.sql
+++ Assignment/Lab02-DDL-Including constraints.sql
@@ -1,78 +1,81 @@
--- Question #231
-create table MEMBER(
-  member_id int(10) primary key,
-  last_name varchar(25) not null,
-  first_name varchar(25),
-  address varchar(100),
-  city varchar(30)
+-- Question #233
+create table title(
+  title_id int primary key,
+  title varchar(60) not null,
+  description varchar(400) not null,
+  rating varchar(4),
+  category varchar(20),
+  release_date date
 );
 
--- Question #1570
-alter table lab_emp
-add primary key (id);
+-- Question #234
+create table title(
+  title_id int,
+  title varchar(60) not null,
+  description varchar(400) not null,
+  rating varchar(4),
+  category varchar(20),
+  release_date date,
+  primary key `title_title_id` (title_id)
+);
 
--- Question #292
-alter table lab_location
-add primary key (location_id);
+-- Question #236
+create table title_copy( 
+  copy_id int primary key, 
+  title_id int not null, 
+  status varchar(15) not null, 
+  foreign key (title_id) 
+  references title(title_id)
+);
 
--- Question #1571
-alter table lab_location
-add primary key (location_id);
-alter table lab_emp
-add constraint lab_emp_fk
-    foreign key (location_id)
-    references lab_location(location_id);
+-- Question #237
+create table title_copy(
+  copy_id int not null,
+  title_id int not null,
+  status varchar(15) not null,
+  primary key `title_copy_title_id` (copy_id),
+  foreign key (title_id)
+  references title(title_id)
+  on delete cascade
+);
 
--- Question #1572
-alter table lab_location
-add constraint loc_name_un
-    unique (location_name);
+-- Question #2441
+alter table title
+add constraint title_un
+    unique (title);
 
--- Question #1573
-alter table lab_emp
-modify last_name varchar(25) not null;
-alter table lab_emp modify
-first_name varchar(25) not null;
+-- Question #2442
+alter table title
+modify release_date date not null;
 
--- Question #2447
-alter table countries
-drop foreign key countries_regions_region_id;
-alter table regions
+-- Question #2443
+alter table title
+add constraint title_cat_fk
+	  foreign key (category_id)
+	  references title_category(title_category_id);
+
+-- Question #2444
+alter table title
 drop primary key;
 
--- Question #2448
-alter table locations
-drop foreign key locations_countries_country_id;
+-- Question #2445
+alter table title
+drop index title_un;
 
--- Question #2449
-alter table employees
-drop foreign key employees_departments_department_id;
-alter table job_history
-drop foreign key job_history_departments_department_id;
-drop table departments;
-
--- Question #2450
-set foreign_key_checks = 0;
-drop table regions;
-set foreign_key_checks = 1;
-
--- Question #2451
-alter table countries drop foreign key countries_regions_region_id;
-drop table regions;
-
--- Question #1548
+-- Question #2446
 alter table employees
 drop foreign key employees_jobs_job_id;
-alter table employees
-drop column job_id;
-describe employees;
+alter table job_history
+drop foreign key job_history_jobs_job_id;
+drop table jobs;
 
--- Question #3110
-create table MEMBER2(
-  member_id int(10),
-  last_name varchar(25) not null,
-  first_name varchar(25),
-  address varchar(100),
-  city varchar(30),
-  primary key (member_id)
-);
+-- Question #2453
+alter table locations
+drop foreign key locations_countries_country_id;
+drop table countries;
+
+-- Question #1551
+alter table title_copy
+drop foreign key title_copy_ibfk_1;
+alter table title_copy
+drop column title_id;
