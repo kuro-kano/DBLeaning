dbc1d80326fc4076b3a697ce83838b5be039b273
Ka-no <138846884+kuro-kano@users.noreply.github.com>
2024-09-25 11:18:33 +0700

    feat: Update LAB05-SQL SELECT CONDITION

--- Assignment/LAB05-SQL SELECT CONDITION.sql
+++ Assignment/LAB05-SQL SELECT CONDITION.sql
@@ -33,3 +33,38 @@ where city like "S______" and state_province is not null;
 -- Question #1704
 select street_address, country_id from locations
 where postal_code like "1%0";
+
+-- Question #1705
+select concat(first_name, ' ', last_name) 'full name', phone_number 'phone' from employees
+where commission_pct is not null and commission_pct > 0;
+
+-- Question #1706
+select first_name, last_name, salary from employees
+where job_id not in ("IT_PROG", "AD_VP", "AD_PRES") order by job_id desc;
+
+-- Question #2585
+select department_name from departments
+where department_name like "IT%";
+
+-- Question #2587
+select first_name, last_name, phone_number from employees
+where phone_number like "515%" or last_name like "_e%";
+
+-- Question #2588
+select employee_id, end_date, job_id from job_history
+where end_date < "2000%";
+
+-- Question #2589
+select employee_id, first_name, last_name, commission_pct + 0.5 as `New Commission` from employees
+where commission_pct + 0.5 > 0.7 order by `New Commission` desc;
+
+-- Question #2590
+select location_id, street_address, state_province from locations
+where country_id != 'CN' and state_province is not null;
+
+-- Question #2591
+select concat(street_address, '_', city) Address from locations
+where country_id not in ('CA', 'CN', 'CH');
+
+-- Question #2668
+select TradeName, GenericMark from medicine where TradeName like '%2.5$%' escape '$';
